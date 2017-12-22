c171020d16ec363798d593362b72f9948ebcd8aa
dunkthelunk <prudhvi_porandla@icloud.com>
2017-12-23 02:08:46 +0530

    1.22

---
+++ 1.22.scm
@@ -0,0 +1,30 @@
+(define (divides? a b) (= (remainder b a) 0))
+(define (square a) (* a a))
+(define (find-divisor n test-divisor) (cond ((> (square test-divisor) n) n)
+                                            ((divides? test-divisor n) test-divisor)
+                                            (else (find-divisor n (+ test-divisor 1)))))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(define (start-prime-test n start-time) 
+  (if (prime? n) (report-prime (- (runtime) start-time)))) 
+
+(define (timed-prime-test n) (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (next-odd n)
+  (if (= (remainder n 2) 0) (+ n 1) (+ n 2)))
+
+(define (primes-in-range start end)
+    (timed-prime-test start)
+    (if (< (next-odd start) end) (primes-in-range (next-odd start) end)))
+
+
