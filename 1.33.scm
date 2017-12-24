2e4ad9da9de7b8e70e290060358f2c36f323b204
dunkthelunk <prudhvi_porandla@icloud.com>
2017-12-25 00:47:55 +0530

    change procudure name

--- 1.33.scm
+++ 1.33.scm
@@ -1,9 +1,9 @@
 ; 1.33.recursive
-(define (accumulate combiner null-value term a next b valid?)
+(define (filtered-accumulate combiner null-value term a next b valid?)
   (if (> a b) null-value (if (valid? a) (combiner (term a) (accumulate combiner null-value term (next a) next b valid?)) (accumulate combiner null-value term (next a) next b valid?))))
 
 ; 1.33.iterative
-(define (accumulate combiner null-value term a next b)
+(define (filtered-accumulate combiner null-value term a next b)
   (define (accumulate-it a acc)
     (if (> a b) acc (if (valid? a) (accumulate-it (next a) (combiner (term a) acc)) (accumulate-it (next a) acc))))
   (accumulate-it a null-value))
