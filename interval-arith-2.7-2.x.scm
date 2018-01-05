884e404deb5798ba1742c815d913a699ff65df67
dunkthelunk <prudhvi.porandla@icloud.com>
2018-01-06 02:31:19 +0530

    interval-arith: 2.10 div when interval spans 0

--- interval-arith-2.7-2.x.scm
+++ interval-arith-2.7-2.x.scm
@@ -23,7 +23,11 @@
         (p4 (* (upper-bound x) (upper-bound y))))
     (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
 
-(define (div-interval x y)
-  (mult-interval x (make-interval (/ 1.0 (upper-bound y)) (/ 1.0 (lower-bound y)))))
+(define (div-interval x y) 
+  (if (>= 0 (* (lower-bound y) (upper-bound y))) 
+    (error "Division error (interval spans 0)" y) 
+    (mul-interval x  
+                  (make-interval (/ 1. (upper-bound y)) 
+                                 (/ 1. (lower-bound y)))))) 
 
 
