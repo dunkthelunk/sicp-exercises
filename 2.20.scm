7046bb2a29df970407dea991e1e261352b638a3a
dunkthelunk <prudhvi.porandla@icloud.com>
2018-01-07 01:00:41 +0530

    2.20 - indent if

--- 2.20.scm
+++ 2.20.scm
@@ -3,5 +3,9 @@
 
 (define (same-parity first . rest)
   (define (same-parity-iter items same-par-items)
-    (if (null? items) (reverse same-par-items) (same-parity-iter (cdr items) (if (same-par first (car items)) (cons (car items) same-par-items) same-par-items))))
+    (if (null? items) 
+      (reverse same-par-items) 
+      (same-parity-iter (cdr items) (if (same-par first (car items)) 
+                                      (cons (car items) same-par-items) 
+                                      same-par-items))))
   (same-parity-iter rest (list first)))
