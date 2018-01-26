33cabf80956e7e64ee10dba3b8e4afda53a8361f
dunkthelunk <prudhvi.porandla@icloud.com>
2018-01-26 20:08:19 +0530

    2.35 - one more impl

--- 2.35.scm
+++ 2.35.scm
@@ -1,2 +1,5 @@
 (define (count-leaves t)
-(accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1) t))))
+  (accumulate + 0 (map (lambda (x) (if (pair? x) (count-leaves x) 1) t))))
+
+(define (count-leaves t)
+  (accumulate (lambda (x y) (+ (length x) y)) 0 (map enumerate-tree t)))
